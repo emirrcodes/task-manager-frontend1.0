de9a2162284269c2029160c17afbd39e47f80222
emirarslan1 <108698362+emirarslan1@users.noreply.github.com>
2025-02-04 16:48:08 +0300

    Update api.ts
    backend url changed

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Task from "../types/Task";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: 'https://task-manager-backend1-0.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
