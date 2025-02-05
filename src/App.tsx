644e192fec9a48c1cb78281608b438ad9cb60609
emirarslan1 <emirarslanonline@gmail.com>
2025-02-05 13:13:23 +0300

    frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import React, {useState} from "react";
-import { HashRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { HashRouter as Router, Navigate, Route, Routes } from "react-router-dom";
 import TaskList from "./components/TaskList";
-import handleDeleteUser from "./components/TaskList";
 import LoginForm from "./components/LoginForm";
 import RegisterForm from "./components/RegisterForm";
 
 const App: React.FC = () => {
-    const isLoggedIn = !!localStorage.getItem('token');
+    const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("token"));
+
+    useEffect(() => {
+        setIsLoggedIn(!!localStorage.getItem("token"));
+    }, []);
 
     const handleLogout = () => {
-        localStorage.removeItem('token');
+        localStorage.removeItem("token");
+        setIsLoggedIn(false); // State'i güncelle
         window.location.href = "/";
-    }
+    };
 
     console.log("Rendering App, isLoggedIn:", isLoggedIn);
 
@@ -19,25 +23,15 @@ const App: React.FC = () => {
         <Router>
             <div>
                 {isLoggedIn && (
-                    <button onClick={handleLogout} style = {{margin : "10px", color: "red"}}>
+                    <button onClick={handleLogout} style={{ margin: "10px", color: "red" }}>
                         Log Out
                     </button>
                 )}
             </div>
             <Routes>
-                <Route 
-                path="/"
-                element={isLoggedIn ? <Navigate to="/tasks" /> : <LoginForm />}
-                />
-                <Route 
-                path="/tasks"
-                element={isLoggedIn ? <TaskList /> : <Navigate to="/" />}
-                />
-
-                <Route 
-                    path="/register" 
-                    element={<RegisterForm />} // /register rotası eklendi.
-                />
+                <Route path="/" element={isLoggedIn ? <TaskList /> : <LoginForm />} />
+                <Route path="/tasks" element={isLoggedIn ? <TaskList /> : <Navigate to="/" />} />
+                <Route path="/register" element={<RegisterForm />} />
             </Routes>
         </Router>
     );
